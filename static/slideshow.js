654753ca2b0aeab9811c81e1a04962ce172782e0
coderabbyte <elementalhare@gmail.com>
2023-10-10 05:34:54 +0000

    fixed firefox overflow not working

--- static/slideshow.js
+++ static/slideshow.js
@@ -1,4 +1,4 @@
-/*https://www.w3schools.com/howto/howto_js_slideshow.asp*/
+/*boiler plate from: https://www.w3schools.com/howto/howto_js_slideshow.asp*/
 let slideIndex = 1;
 
 function plusSlides(n) {
